564e4f1077344d43cc4b6cef0dd69a989c511a67
jo-price <188100478+jo-price@users.noreply.github.com>
2025-03-25 20:29:12 -0500

    Add jsx files for Planet 2 and ReturnToShip button

---
+++ how-not-to-die/src/components/PlanetTwoLandscape.jsx
@@ -0,0 +1,2 @@
+// file path: how-not-to-die/src/components/PlanetTwoLandscape.jsx
+// Shows the second planet landscape
---
+++ how-not-to-die/src/components/ReturnToShipButton.jsx
@@ -0,0 +1,2 @@
+// file path: how-not-to-die/src/components/ReturnToShipButton.jsx
+// Button to return to homepage (console dashboard)

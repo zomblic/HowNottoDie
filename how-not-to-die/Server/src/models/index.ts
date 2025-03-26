5d01b4b64093402ef4c32c9ae478cca63f245bfc
Blakenb <blakenb87@gmail.com>
2025-03-25 21:36:55 -0500

    added to the files using some promt work, and from looking over 25 from the class repo

---
+++ how-not-to-die/Server/src/models/index.ts
@@ -0,0 +1,6 @@
+import sequelize from '../config/connection.js'
+import { UserFactory } from './user.js';
+
+const User = UserFactory(sequelize);
+
+export { User };
---
+++ how-not-to-die/Server/src/routes/index.ts
@@ -0,0 +1,4 @@
+import authRoutes from './auth-routes';
+import userRoutes from './user-routes';
+
+export { authRoutes, userRoutes };

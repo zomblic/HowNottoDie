7f939d14cc07fbd66e851784bae4bf6f6c29db3e
Alexis Menendez <alexis.menendez@austincc.edu>
2025-03-25 21:14:12 -0500

    setting up pages a bit

--- how-not-to-die/src/pages/PersonalLogs.jsx
+++ how-not-to-die/src/pages/PersonalLogs.jsx
@@ -2,39 +2,15 @@
 // View/edit user’s own logs
 
 import React from 'react';
-import styles from "../assets/css/PersonalLogs.module.css";
-
-// below is an example of how to set your jsx page up
-// so that it grabs whatever component you need for this page /Alexis
-
-import PersonalLogs from '../components/ExampleComponent'; 
+import ConsoleLayout from '../components/ConsoleLayout';
 
 const PersonalLogs = () => {
   return (
-    <div>
-      <ExampleComponent />
-    </div>
+    <ConsoleLayout>
+      <h2>Welcome, Commander</h2>
+      <p>VERA is watching...</p>
+    </ConsoleLayout>
   );
 };
 
 export default PersonalLogs;
-
-// It can also look like this:
-
-//const Resume = () => {
-//    return (
-//      <ExampleComponent>
-//        <div className={styles.resumeContent}>
-//          <a href="public/assets/pdf/TechResume2025.pdf" download="TechResume2025.pdf">
-//            Thanks for stopping by!
-//          </a>
-//          <a href="public/assets/pdf/TechResume2025.pdf" download="TechResume2025.pdf">
-//            Click here to download my resume as a PDF
-//          </a>
-//          <div className={styles.resumeImage}>
-//            <img src="/assets/images/TechResume2025.webp" alt="Resume" />
-//          </div>
-//        </div>
-//      </ExampleComponent>
-//    );
-//  };  

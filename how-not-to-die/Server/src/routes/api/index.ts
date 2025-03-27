5c58671a9820db09ea9661139872e1a682f5973c
Toonz <johnnyt3.2014@gmail.com>
2025-03-26 22:56:20 -0400

    updated api index

--- how-not-to-die/Server/src/routes/api/index.ts
+++ how-not-to-die/Server/src/routes/api/index.ts
@@ -1,8 +1,20 @@
+//Johnnie's code
 import { Router } from 'express';
-import { userRouter } from './user-routes.js';
+import userRouter from './user-routes.js'; // Use default import for userRouter
 
 const router = Router();
 
 router.use('/users', userRouter);
 
 export default router;
+
+
+
+// import { Router } from 'express';
+// import { userRouter } from './user-routes.js';
+
+// const router = Router();
+
+// router.use('/users', userRouter);
+
+// export default router;

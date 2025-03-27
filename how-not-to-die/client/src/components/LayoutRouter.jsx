45f1d92afb84f702b83ac1664a3ea13e9cb3872d
Alexis Menendez <alexis.menendez@austincc.edu>
2025-03-26 23:26:22 -0500

    started working on galaxy map

--- how-not-to-die/client/src/components/LayoutRouter.jsx
+++ how-not-to-die/client/src/components/LayoutRouter.jsx
@@ -9,12 +9,16 @@ import Dashboard from '../pages/Dashboard';
 import SurvivalGuide from '../pages/SurvivalGuide';
 import PersonalLogs from '../pages/PersonalLogs';
 import PlanetTravel from '../pages/PlanetTravel';
+import Holomap from '../pages/Holomap';
 import PlanetOne from '../pages/PlanetOne';
 import PlanetTwo from '../pages/PlanetTwo';
 
 const LayoutRouter = () => {
   const location = useLocation();
-  const isPlanetView = location.pathname === '/planetone' || location.pathname === '/planettwo';
+  const isPlanetView =
+    location.pathname === '/planetone' ||
+    location.pathname === '/planettwo' ||
+    location.pathname === '/holomap';
 
   return (
     <>
@@ -22,6 +26,7 @@ const LayoutRouter = () => {
         <Routes>
           <Route path="/planetone" element={<PlanetOne />} />
           <Route path="/planettwo" element={<PlanetTwo />} />
+          <Route path="/holomap" element={<Holomap />} />
         </Routes>
       ) : (
         <ConsoleLayout>
--- how-not-to-die/client/src/pages/PlanetTravel.jsx
+++ how-not-to-die/client/src/pages/PlanetTravel.jsx
@@ -1,12 +1,17 @@
 // file path: how-not-to-die/src/pages/PlanetTravel.jsx
 
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import styles from '../assets/css/PlanetTravel.module.css';
 
 const PlanetTravel = () => {
+  const navigate = useNavigate();
+
   return (
     <>
-      <h2>Welcome, Commander</h2>
-      <p>VERA is watching...</p>
+      <button className={styles.holomapButton} onClick={() => navigate('/holomap')}>
+        Initiate Holomap Projection?
+      </button>
     </>
   );
 };

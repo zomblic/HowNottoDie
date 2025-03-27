c9587d4064e5f827ab4625f812dd96e65abf148a
zomblic <zomblic@hotmail.com>
2025-03-27 18:14:05 -0400

    tsconfig currently being debugged as we don't have dist in server folder.

---
+++ how-not-to-die/Server/src/config/connection.js
@@ -0,0 +1,6 @@
+import { Sequelize } from 'sequelize';
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: 'localhost',
+    dialect: 'postgres',
+});
+export default sequelize;
---
+++ how-not-to-die/Server/src/routes/api/user-routes.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import User from './user';
+const router = express.Router();
+router.get('/', async (req, res) => {
+    const users = await User.findAll();
+    res.json(users);
+});
+router.get('/:id', async (req, res) => {
+    const user = await User.findByPk(req.params.id);
+    if (user) {
+        res.json(user);
+    }
+    else {
+        res.status(404).json({ error: 'User not found' });
+    }
+});
+export default router;

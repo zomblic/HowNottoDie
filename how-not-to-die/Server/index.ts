c6f9b4f6fc0b370773f1fba706e7adea86b9f6d5
Blakenb <blakenb87@gmail.com>
2025-03-30 15:46:58 -0500

    we have a working server

--- how-not-to-die/Server/index.ts
+++ how-not-to-die/Server/index.ts
@@ -1,8 +1,8 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import authRoutes from './src/routes/auth-routes.js';
-import userRoutes from './src/routes/api/user-routes.js';
-import sequelize from './src/config/connection.js';
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./src/routes/auth-routes.js";
+import userRoutes from "./src/routes/api/user-routes.js";
+import sequelize from "./src/config/connection.js";
 
 dotenv.config();
 
@@ -10,8 +10,8 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use('/auth', authRoutes);
-app.use('/users', userRoutes);
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
 
 sequelize.sync().then(() => {
   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

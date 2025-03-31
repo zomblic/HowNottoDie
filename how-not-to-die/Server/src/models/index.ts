e466416e3fa769fb9bac2ef7602b79740a355c5d
zomblic <zomblic@hotmail.com>
2025-03-31 18:14:19 -0400

    continuing debugging. It's the fun job!

--- how-not-to-die/Server/src/models/index.ts
+++
@@ -1,6 +0,0 @@
-//import sequelize from '../config/connection.js'
-import UserFactory from './user.js';
-
-const User = new UserFactory();
-
-export { User };
--- how-not-to-die/Server/src/routes/index.ts
+++
@@ -1,6 +0,0 @@
-import authRoutes from './auth-routes.js';
-import userRoutes from '../routes/api/user-routes.js';
-
-
-
-export { authRoutes, userRoutes };
